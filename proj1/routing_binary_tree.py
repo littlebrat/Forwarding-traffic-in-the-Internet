21532ee80e5c737cdeb6b252cca902d5036efdd5
davidfialho <fialho.david@gmail.com>
2015-09-25 18:01:40 +0100

    defined the interface for the routing binary tree

---
+++ proj1/routing_binary_tree.py
@@ -0,0 +1,13 @@
+from proj1.prefix import Prefix
+
+
+class RoutingBinaryTree:
+
+    def insert(self, prefix):
+        pass
+
+    def remove(self, prefix):
+        pass
+
+    def lookup(self, prefix):
+        pass
