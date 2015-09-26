bdc1fcab8876e296e4a1a35abd3fa7b1bae343bc
Nuno Mendes <mrgugas1993@gmail.com>
2015-09-26 12:37:11 +0100

    comments added

--- proj1/binarytree.py
+++ proj1/binarytree.py
@@ -28,15 +28,24 @@ class BinaryTree:
         cur_node.set_next_hop(next_hop)
 
     def lookup(self,prefix):
+        # starting point for the search
         cur_node = self.root
         for bit in prefix:
             if bit is '1':
+                # move to the right
                 if cur_node.right() is None:
+                    # if there is not any node right, longest prefix has been found
                     return cur_node.getHop()
+                    # return the nextHop for this ip
+                # move right
                 cur_node = cur_node.right()
             else:
+                # move to the left
                 if cur_node.left() is None:
+                    # if there is not any node left, longest prefix has been found
                     return cur_node.getHop()
+                    # return the nextHop for this ip
+                # move left
                 cur_node = cur_node.left()
 
 
