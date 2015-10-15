d452c24966af82f1b8301810056f3cc8bdf26609
David Fialho <fialho.david@gmail.com>
2015-10-15 22:33:10 +0100

    added test for the test_1.txt

--- proj1/tests/test_binary_tree.py
+++ proj1/tests/test_binary_tree.py
@@ -1,3 +1,4 @@
+from proj1.binarytree import BinaryTree
 from proj1.prefix import Prefix
 import proj1.tests.trees as trees
 
@@ -7,3 +8,20 @@ btree.print()
 print()
 btree.delete(Prefix(''))
 btree.print()
+print()
+
+print("file test_1.txt")
+btree = BinaryTree.from_file('/home/david/Development/IST/ADRC/Mini-Projecto-1/proj1/test_files/test_1.txt')
+btree.print()
+print()
+
+btree.delete(Prefix('001'))
+btree.delete(Prefix('01'))
+btree.delete(Prefix('10'))
+btree.delete(Prefix('11'))
+
+btree.print()
+print()
+btree.delete(Prefix('000'))
+btree.print()
+print()
