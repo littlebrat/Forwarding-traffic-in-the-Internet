817cc5043f22bc2bccc33f0ae18b69f5e5015e3f
David Fialho <fialho.david@gmail.com>
2015-10-16 09:08:15 +0100

    introduced the pretty table module to print the prefix table

--- proj1/binarytree.py
+++ proj1/binarytree.py
@@ -3,6 +3,7 @@ from proj1.node import Node
 from proj1.prefix import Prefix
 from proj1.ip_address import to_binary
 import proj1.ip_address as ip
+from prettytable import PrettyTable
 
 
 class BinaryTree:
@@ -225,19 +226,22 @@ class BinaryTree:
         BinaryTree.__print_node(self.root, 0)
 
     def print_table(self):
-        BinaryTree.__print_table_node(self.root, '')
+        table = PrettyTable(["Prefixes", "Next-Hops"])
+        table.align["Prefixes"] = "l"
+        BinaryTree.__print_table_node(self.root, '', table)
+        print(table)
 
     @staticmethod
-    def __print_table_node(node, bits):
+    def __print_table_node(node, bits, table):
         if node is not None:
             if node.next_hop:
-                print(bits, node.next_hop)
+                table.add_row([bits, node.next_hop])
 
             # print left node
-            BinaryTree.__print_table_node(node.left, bits + '0')
+            BinaryTree.__print_table_node(node.left, bits + '0', table)
 
             # print right node
-            BinaryTree.__print_table_node(node.right, bits + '1')
+            BinaryTree.__print_table_node(node.right, bits + '1', table)
 
     @staticmethod
     def __print_node(node, level):
